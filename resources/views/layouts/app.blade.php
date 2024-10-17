d5e11f1de8353b8df3f200e30f80c282a703ddd9
LubnaAjnal <lubna_ajnal@secab.org>
2024-10-17 16:13:50 +0530

    added yield to mainContent

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+{{-- includer header file --}}
+@include('layouts.header');
+
+{{-- includes menu sidebar file --}}
+@include('layouts.menu');
+
+{{-- includes body content file --}}
+@include('layouts.mainContent');
+
+{{-- includes footer file --}}
+@include('layouts.footer');
+
+
+                
+            
+        
+    
