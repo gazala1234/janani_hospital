cb5cf7f082d7ae511fc893f6fd904d5c87f1352e
gazala1234 <ustadgazal@gmail.com>
2024-10-18 10:16:32 +0530

    guest dashboard

--- resources/views/layouts/mainContent.blade.php
+++ resources/views/layouts/mainContent.blade.php
@@ -1,16 +1,9 @@
 <div id="main">
-    <header class="mb-3">
-        <a href="#" class="burger-btn d-block d-xl-none">
-            <i class="bi bi-justify fs-3"></i>
-        </a>
-    </header>
 
     <div class="page-heading">
-        <h3>@yield('page-heading')</h3> 
+        <h3>@yield('page-heading')</h3>
         {{-- //Profile Statistics --}}
     </div>
     <div class="page-content">
         @yield('page-content')
     </div>
-
-    
